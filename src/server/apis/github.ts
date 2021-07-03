8749ccb2fe1a71bab27b7a2b6f7170b945505286
Matjaž Čelan <matjazcelan.github@gmail.com>
2021-07-04 01:45:17 +0200

    updated api fetching

--- src/server/apis/github.ts
+++ src/server/apis/github.ts
@@ -1,28 +1,33 @@
 import { githubMetrics } from './../../types/metrics';
-import axios from 'axios';
+import axios, { AxiosResponse } from 'axios';
+
+type GithubResponse = Omit<githubMetrics, 'branches_count'> & {
+  branches_url: string;
+};
 
 export const fetchGithubData = async (
   accessToken: string,
   tokenType: string
 ): Promise<githubMetrics> => {
-  const res = await axios.get(
+  const res = await axios.get<{}, AxiosResponse<GithubResponse>>(
     'https://api.github.com/repos/CelanMatjaz/Databox',
-    { headers: { Authorization: `${tokenType} ${accessToken}` } }
+    {
+      headers: { Authorization: `${tokenType} ${accessToken}` },
+    }
   );
 
-  const {
-    forks_count,
-    open_issues,
-    default_branch,
-    language,
-    subscribers_count,
-  } = res.data;
+  const { forks_count, open_issues, watchers_count, subscribers_count } =
+    res.data;
+
+  const branchesRes = await axios.get<{}, AxiosResponse<[]>>(
+    res.data.branches_url.split('{')[0]
+  );
 
   return {
     forks_count,
     open_issues,
-    default_branch,
-    language,
+    branches_count: branchesRes.data.length,
+    watchers_count,
     subscribers_count,
   };
 };
--- src/server/apis/weather.ts
+++ src/server/apis/weather.ts
@@ -5,44 +5,33 @@ const city = 'maribor';
 const API_KEY = process.env.WEATHER_API_KEY;
 
 export const fetchWeatherData = async (): Promise<weatherMetrics> => {
-  try {
-    const res = await axios.get<
-      {},
-      AxiosResponse<{
-        weather: { main: string; description: string };
-        main: { temp: number };
-        visibility: number;
-        wind: { speed: number };
-      }>
-    >(`http://api.openweathermap.org/data/2.5/weather`, {
-      params: { q: city, appid: API_KEY, units: 'metric' },
-      headers: {
-        'content-type': 'application/json',
-      },
-    });
+  const res = await axios.get<
+    {},
+    AxiosResponse<{
+      main: { temp: number; pressure: number; humidity: number };
+      visibility: number;
+      wind: { speed: number };
+    }>
+  >(`http://api.openweathermap.org/data/2.5/weather`, {
+    params: { q: city, appid: API_KEY, units: 'metric' },
+    headers: {
+      'content-type': 'application/json',
+    },
+  });
 
-    const {
-      weather: { main, description },
-      main: { temp },
-      visibility,
-      wind: { speed },
-    } = res.data;
+  console.log(res.data);
 
-    return {
-      weather: 'sunny',
-      weather_description: 'clear',
-      temperature: 25,
-      visibility: 10000,
-      wind_speed: 0.45,
-    };
-  } catch {
-    console.log('couldnt get weather data, adding custom data');
-    return {
-      weather: 'sunny',
-      weather_description: 'clear',
-      temperature: 25,
-      visibility: 10000,
-      wind_speed: 0.45,
-    };
-  }
+  const {
+    main: { temp, pressure, humidity },
+    visibility,
+    wind: { speed },
+  } = res.data;
+
+  return {
+    pressure,
+    humidity,
+    visibility,
+    temperature: temp,
+    wind_speed: speed,
+  };
 };
