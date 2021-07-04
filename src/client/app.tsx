39cc9120dec71c16cf0520046b5f8c2665f1a373
Matjaž Čelan <matjazcelan.github@gmail.com>
2021-07-04 22:44:07 +0200

    added loading indication

--- src/client/app.tsx
+++ src/client/app.tsx
@@ -4,30 +4,43 @@ import Card from './components/Card';
 
 export const App: React.FC = () => {
   const [data, setData] = React.useState<Data[]>([]);
+  const [loading, setLoading] = React.useState(false);
+
+  const fetchData = async () => {
+    setLoading(true);
+    const res = await fetch('/data');
+    const data = await res.json();
+    setData(data);
+    setLoading(false);
+  };
 
   React.useEffect(() => {
-    const fetchData = async () => {
-      const res = await fetch('/data');
-      const data = await res.json();
-
-      setData(data);
-    };
-
     fetchData();
   }, []);
 
   return (
     <div className='container'>
+      <div>
+        <button id='update-button' onClick={fetchData}>
+          {' '}
+          Update
+        </button>
+      </div>
       <h1
         style={{
           borderBottom: '2px solid #313131',
         }}>
         Data
       </h1>
+
       <div className='data'>
-        {data.map((card, i) => (
-          <Card {...card} key={i} />
-        ))}
+        {loading ? (
+          'Loading...'
+        ) : data.length > 0 ? (
+          data.map((card, i) => <Card {...card} key={i} />)
+        ) : (
+          <h2>No data to display...</h2>
+        )}
       </div>
     </div>
   );
