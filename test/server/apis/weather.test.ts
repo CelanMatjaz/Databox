c0b9c1c3eb187ac9c3341e5943dfdf08cf227a57
Matjaž Čelan <matjazcelan.github@gmail.com>
2021-07-05 18:32:36 +0200

    added some tests

---
+++ test/server/apis/weather.test.ts
@@ -0,0 +1,31 @@
+import { fetchWeatherData } from './../../../src/server/apis/weather';
+import dotenv from 'dotenv';
+import { expect } from 'chai';
+
+dotenv.config();
+
+import config, { Config } from '../../../src/server/config';
+
+describe('weather api', () => {
+  it('should fetch weather data from the api', async () => {
+    const data = await fetchWeatherData(config);
+    expect(data).to.not.be.null;
+  });
+
+  it('should return all the required fields for metrics', async () => {
+    const data = await fetchWeatherData(config);
+    const { pressure, humidity, visibility, temperature, wind_speed } = data;
+
+    expect(pressure).to.not.be.undefined;
+    expect(humidity).to.not.be.undefined;
+    expect(visibility).to.not.be.undefined;
+    expect(temperature).to.not.be.undefined;
+    expect(wind_speed).to.not.be.undefined;
+  });
+
+  it('should throw an error', async () => {
+    try {
+      expect(await fetchWeatherData({} as Config)).to.throw;
+    } catch {}
+  });
+});
